22d147b03c3e8f397ebfa760b875d5f09be8c9cc
Diego Maia <diego.maia@digibee.com.br>
2021-03-06 14:35:08 -0300

    add first commit

--- apis/projects.go
+++ apis/projects.go
@@ -21,7 +21,7 @@ func GetProjects(w http.ResponseWriter, r *http.Request) {
 	var projects []models.Project
 
 	var collection = client.Database("bulman").Collection("projects")
-	cur, err := collection.Find(context.TODO(), bson.M{}).SetSort(bson.M{{"created", -1}})
+	cur, err := collection.Find(context.TODO(), bson.M{}).SetSort(bson.M{{"created_at", -1}})
 	if err != nil {
 		helper.GetError(err, w)
 		return
